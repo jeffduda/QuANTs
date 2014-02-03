c184b63d1c63663d784f7802577cd73c72f2eb7c
jeffduda <jeff.duda@gmail.com>
2014-02-03 14:43:24 -0500

    ADD: more dicom info added

--- Scripts/Dicom/dicominfo.py
+++ Scripts/Dicom/dicominfo.py
@@ -9,6 +9,7 @@ def get_filepaths(directory):
   """
 
   file_paths = []  # List which will store all of the full filepaths.
+  sequences = []
 
   # Walk the tree.
   for root, directories, files in os.walk(directory):
@@ -23,12 +24,13 @@ def get_filepaths(directory):
       reader.SetFileName(filepath)
       if (reader.Read()):
         file = reader.GetFile()
-        print filepath
+        #print filepath
 
         # Retrieve data set
         dataSet = file.GetDataSet()
         if ( dataSet.FindDataElement(gdcm.Tag(0x0008, 0x103e)) ):
           print str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x103e)).GetValue() )
+          sequences.append( str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x103e)).GetValue() ) )
         
         
         # Iterate through the DICOMDIR data set
@@ -52,11 +54,109 @@ def get_filepaths(directory):
        #         print value
 
         file_paths.append(filepath)
-
-
+ 
+  sequences  = sorted(set(sequences))
   return file_paths  # Self-explanatory.
 
 
+
+def print_dicom_info(filenames):
+  """Print info related to each dicom file
+  """
+  for filename in filenames:
+    
+    # Read file
+    reader = gdcm.Reader()
+    reader.SetFileName(filename)
+    if (reader.Read()):
+      file = reader.GetFile()
+      #print filepath
+
+      # Retrieve data set
+      dataSet = file.GetDataSet()
+      if ( dataSet.FindDataElement(gdcm.Tag(0x0008, 0x103e)) ):
+        filesetid = str( dataSet.GetDataElement(gdcm.Tag(0x0004,0x1130)).GetValue() )
+        seq = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x103e)).GetValue() )
+        studydate = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0020)).GetValue() )
+        studytime = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0030)).GetValue() )
+        seriestime = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0031)).GetValue() )
+        acquisitiontime = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0032)).GetValue() )
+        print( seq + " " + studydate + " " + studytime + " " + seriestime + " " + acquisitiontime )
+
+        
+
+def get_sequences(filenames):
+  """This function will generate the sequence names of dicom files
+  """
+
+  sequences = []
+  for filename in filenames:
+
+    # Read file
+    reader = gdcm.Reader()
+    reader.SetFileName(filename)
+    if (reader.Read()):
+      file = reader.GetFile()
+      #print filepath
+
+      # Retrieve data set
+      dataSet = file.GetDataSet()
+      if ( dataSet.FindDataElement(gdcm.Tag(0x0008, 0x103e)) ):
+        print str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x103e)).GetValue() )
+        sequences.append( str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x103e)).GetValue() ) )
+
+      
+  return sequences
+
+def FindDicomStudies(filenames):
+
+  studies = []
+  for filename in filenames:
+
+    # Read file
+    reader = gdcm.Reader()
+    reader.SetFileName(filename)
+    if (reader.Read()):
+      file = reader.GetFile()
+      #print filepath
+
+      # Retrieve data set
+      dataSet = file.GetDataSet()
+      if ( dataSet.FindDataElement(gdcm.Tag(0x0008, 0x103e)) ):
+        studydate = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0020)).GetValue() )
+        studytime = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0030)).GetValue() )
+        simpletime = studytime[:studytime.find( "." )]
+        studies.append( studydate + "_" + simpletime )
+
+      
+  return studies
+
+
+def FindSeriesByStudy(filenames, study):
+
+  studies = []
+  for filename in filenames:
+
+    # Read file
+    reader = gdcm.Reader()
+    reader.SetFileName(filename)
+    if (reader.Read()):
+      file = reader.GetFile()
+      #print filepath
+
+      # Retrieve data set
+      dataSet = file.GetDataSet()
+      if ( dataSet.FindDataElement(gdcm.Tag(0x0008, 0x103e)) ):
+        studydate = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0020)).GetValue() )
+        studytime = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x0030)).GetValue() )
+        seq = str( dataSet.GetDataElement(gdcm.Tag(0x0008, 0x103e)).GetValue() )
+        simpletime = studytime[:studytime.find( "." )]
+        studyid = studydate + "_" + simpletime 
+        if ( studyid == study ):
+          studies.append(seq)
+
+      
+  return studies
 
 
 
